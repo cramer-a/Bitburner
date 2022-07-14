440afb4d2089bf0de7ab7b5c68b2bcd8118d8202
Alex Cramer <alexcramer@gmx.de>
2022-07-14 21:06:56 +0200

    Working and failing

--- basic_farming_weaken.js
+++ basic_farming_weaken.js
@@ -19,28 +19,28 @@ export async function main(ns) {
 					};
 					var sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					ns.tprint('security difference is ' + sec);
-					while (sec > 0) {
+					if (sec > 0) {
 						ns.tprint('weaken')
 						ns.run('weakening.js', 1, target);
-    					while (ns.isRunning('weakening.js')) await ns.sleep();
+    					while (ns.isRunning('weakening.js')) await ns.sleep(500);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					};
+					} else {};
 					var growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)));;
 					ns.tprint('room for growth is ' + growth_perc);
-					while (growth_perc > 0.25) {
+					if (growth_perc > 0.25) {
 						ns.tprint('Growing');
 						ns.run('growing.js', 1, target);
-    					while (ns.isRunning('growing.js')) await ns.sleep();
+    					while (ns.isRunning('growing.js')) await ns.sleep(500);
 						growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)))
-					};
+					} else {};
 					sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					ns.tprint('security difference is ' + sec);
-					while (sec > 0) {
+					if (sec > 0) {
 						ns.tprint('weaken')
 						ns.run('weakening.js', 1, target);
-    					while (ns.isRunning('weakening.js')) await ns.sleep();
+    					while (ns.isRunning('weakening.js')) await ns.sleep(500);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					};
+					} else {};
 					ns.tprint('hacking ' + target);
 					//await ns.nuke(target);
 					let earned = await ns.hack(target);
