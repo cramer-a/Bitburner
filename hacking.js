8a88367a7b0283cb20a8d78baf0bf792d1418d4b
Alex Cramer <alexcramer@gmx.de>
2022-08-03 20:59:47 +0200

    Buying servers and using them

--- hacking.js
+++ hacking.js
@@ -1,6 +1,5 @@
 /** @param {NS} ns */
 export async function main(ns, target) {
     target = ns.args[0]
-    let retrieved = await ns.hack(target, 1)
-    return retrieved;
+    await ns.hack(target, 1)
 };
