132d6c8487a1e8aabd61bd6a70956db052ccce95
Alex Cramer <alexcramer@gmx.de>
2022-08-04 16:02:51 +0200

    Getting rid of unnecessary terminal prints

--- basic_farming.js
+++ basic_farming.js
@@ -6,45 +6,45 @@ export async function main(ns) {
 		for (let i = 0; i < connectedservers.length; i++) {
 			if (ns.getServerNumPortsRequired(connectedservers.at(i)) <= 1) {
 				let target = connectedservers.at(i);
-				ns.tprint("Target is " + target);
+				ns.print("Target is " + target);
 				let skillgap = ns.getServerRequiredHackingLevel(target) - ns.getHackingLevel() > 0;
 				if (skillgap > 0) {
-					ns.tprint('Skill gap is too high: ' + skillgap)
+					ns.print('Skill gap is too high: ' + skillgap)
 				} else {
 					if (ns.hasRootAccess(target) == true) {
-						ns.tprint('Having root');
+						ns.print('Having root');
 					} else {
-						ns.tprint('No Root');
+						ns.print('No Root');
 						await ns.nuke(target);
-						ns.tprint('Nuked');
+						ns.print('Nuked');
 					};
 					let sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					ns.tprint("security difference is " + sec);
+					ns.print("security difference is " + sec);
 					while (sec > 0) {
-						ns.tprint('weakening')
+						ns.print('weakening')
 						await ns.weaken(target);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					};
 					let growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)));
-					ns.tprint(ns.getServerMoneyAvailable(target) / ns.getServerMaxMoney(target));
-					ns.tprint('room for growth is ' + growth_perc);
+					ns.print(ns.getServerMoneyAvailable(target) / ns.getServerMaxMoney(target));
+					ns.print('room for growth is ' + growth_perc);
 					while (growth_perc > 0.25) {
-						ns.tprint('Growing');
+						ns.print('Growing');
 						await ns.grow(target);
 						growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)));
-						ns.tprint('New room for growth is' + growth_perc);
+						ns.print('New room for growth is' + growth_perc);
 					};
 					sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					ns.tprint("security difference is " + sec);
+					ns.print("security difference is " + sec);
 					while (sec > 0) {
-						ns.tprint('weakening')
+						ns.print('weakening')
 						await ns.weaken(target);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					};
-					ns.tprint('hacking')
+					ns.print('hacking')
 					await ns.nuke(target);
 					let earned = await ns.hack(target);
-					ns.tprint('Cashed out ' + earned);
+					ns.print('Cashed out ' + earned);
 					}
 				}
 			};
--- basic_farming_weaken.js
+++ basic_farming_weaken.js
@@ -6,49 +6,49 @@ export async function main(ns) {
 		for (let i = 0; i < connectedservers.length; i++) {
 			if (ns.getServerNumPortsRequired(connectedservers.at(i)) < 1) {
 				var target = connectedservers.at(i);
-				ns.tprint('Target is ' + target);
+				ns.print('Target is ' + target);
 				let skillgap = ns.getServerRequiredHackingLevel(target) - ns.getHackingLevel();
 				if (skillgap > 0) {
-					ns.tprint('Skill gap is too high: ' + skillgap)
+					ns.print('Skill gap is too high: ' + skillgap)
 				} else {
 					if (ns.hasRootAccess(target) == true) {
-						ns.tprint('Having root');
+						ns.print('Having root');
 					} else {
-						ns.tprint('No Root');
+						ns.print('No Root');
 						ns.nuke(target);
-						ns.tprint('Nuked');
+						ns.print('Nuked');
 					};
 					var sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					ns.tprint('security difference is ' + sec);
+					ns.print('security difference is ' + sec);
 					while (sec > 0) {
-						ns.tprint('weaken')
+						ns.print('weaken')
 						let threading = Math.floor((ns.getServerMaxRam(home)-ns.getServerUsedRam(home))/ ns.getScriptRam('weakening.js'));
 						let pid = ns.run('weakening.js', threading, target);
     					while (ns.isRunning(pid)) await ns.sleep(500);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					};
 					var growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)));
-					ns.tprint('room for growth is ' + growth_perc);
+					ns.print('room for growth is ' + growth_perc);
 					for (let j = 0; j < 10; j++) {
 						if (growth_perc > 0.25) {
-							ns.tprint('Growing');
+							ns.print('Growing');
 							let threading = ((ns.getServerMaxRam(home)-ns.getServerUsedRam(home))/ ns.getScriptRam('growing.js'));
 							let pid = ns.run('growing.js', threading, target);
 							while (ns.isRunning(pid)) await ns.sleep(500);
 							growth_perc = (1-(ns.getServerMoneyAvailable(target)/ns.getServerMaxMoney(target)));
-							ns.tprint('- Round ' + j + '/ - Room for growth now ' + growth_perc + ' -');
+							ns.print('- Round ' + j + '/ - Room for growth now ' + growth_perc + ' -');
 						} else {break};
 					};
 					sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
-					ns.tprint('security difference is ' + sec);
+					ns.print('security difference is ' + sec);
 					while (sec > 0) {
-						ns.tprint('weaken')
+						ns.print('weaken')
 						let threading = ((ns.getServerMaxRam(home)-ns.getServerUsedRam(home))/ ns.getScriptRam('weakening.js'));
 						let pid = ns.run('weakening.js', threading, target);
     					while (ns.isRunning(pid)) await ns.sleep(500);
 						sec = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
 					};
-					ns.tprint('hacking ' + target);
+					ns.print('hacking ' + target);
 					let maxthreading = Math.floor(0.1/ns.hackAnalyze(target));
 					//await ns.nuke(target);
 					let threading = Math.floor((ns.getServerMaxRam(home)-ns.getServerUsedRam(home))/ ns.getScriptRam('hacking.js'));
@@ -56,9 +56,9 @@ export async function main(ns) {
 					let pid = ns.run('hacking.js', Math.min(maxthreading, threading), target);
 					while (ns.isRunning(pid)) await ns.sleep(500);
 					let newavail = ns.getServerMoneyAvailable(target);
-					ns.tprint('Cashed out ' + (available - newavail) + ' out of ' + available + ' available');
+					ns.print('Cashed out ' + (available - newavail) + ' out of ' + available + ' available');
 				};
-			} else {ns.tprint('Too many ports needed for ' + connectedservers.at(i))};
+			} else {ns.print('Too many ports needed for ' + connectedservers.at(i))};
 		};
 	};
 }
